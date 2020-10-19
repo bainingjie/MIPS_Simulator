6440221136677edf871f6844d237b719ac651f5c
白寧杰 <ningjie20081006@gmail.com>
2020-10-20 06:18:19 +0900

    fib done

--- MIPSSimulator_class.cpp
+++ MIPSSimulator_class.cpp
@@ -14,6 +14,8 @@ class MIPSSimulator
         string Registers[32]; //array to store names of registers
         int32_t RegisterValues[32]; //array to store values of registers
 
+        int32_t Memory[1024*100];// memory
+
         int32_t Mode; //to store the Mode of execution
         
         vector<string> InputProgram; //to store the input program, element stands for
@@ -23,8 +25,16 @@ class MIPSSimulator
         int32_t ProgramCounter; 
         int32_t r[3]; //to store the id of operand or constant
 
-        // void add();
+
+        int32_t Instruction_op; 
+        int32_t Instruction_funct;
+
+        void add();
         void addi();
+        void jr();
+        void bne();
+        void lw();
+        void sw();
         // void sub();
         // void mul();
         // void andf();
@@ -32,19 +42,20 @@ class MIPSSimulator
         // void orf();
         // void ori();
         // void nor();
-        // void slt();
+        void slt();
         // void slti();
-        // void lw();
-        // void sw();
+
         // void beq();
-        // void bne();
-        // void j();
+
+        void j();
+        void jal();
 
 
         void ReadInstruction(int32_t line);
-        int32_t ParseInstruction();
+        void ParseInstruction();
         // void ReportError();
-        void ExecuteInstruction(int32_t instruction);
+        void ExecuteInstruction();
+        void PrintRegister();
 
     public:
         MIPSSimulator(int32_t mode, string fileName);
@@ -66,6 +77,7 @@ MIPSSimulator::MIPSSimulator(int32_t mode, string fileName)
     ProgramCounter=0;
     string tempRegisters[]={"zero","at","v0","v1","a0","a1","a2","a3","t0","t1","t2","t3","t4","t5","t6",
     "t7","s0","s1","s2","s3","s4","s5","s6","s7","t8","t9","k0","k1","gp","sp","s8","ra"}; //names of registers
+
     for(int32_t i=0;i<32;i++)
     {
         Registers[i]=tempRegisters[i];
@@ -74,7 +86,10 @@ MIPSSimulator::MIPSSimulator(int32_t mode, string fileName)
     {
         RegisterValues[i]=0; 
     }
-
+    for(int32_t i=0;i<1024*4;i++)
+    {
+        Memory[i]=0; 
+    }
     Mode = mode; 
     ifstream InputFile;
     InputFile.open(fileName.c_str(),ios::in); 
@@ -93,22 +108,22 @@ MIPSSimulator::MIPSSimulator(int32_t mode, string fileName)
     InputFile.close();
 }
 
-
-//function to run the simulator
-void MIPSSimulator::Execute()
+void MIPSSimulator::PrintRegister()
 {
-    char test = getchar(); //to remove effect of pressing enter key while starting
-    cout<<"getchar() in Execute: "<<test <<endl;
-    while(ProgramCounter<NumberOfInstructions) 
-    {
+    // for (int i = 0; i <= 31; i++){
+    //     cout<<Registers[i]<<": "<<RegisterValues[i]<<endl;
+    // }
+    cout<<Registers[2]<<": "<<RegisterValues[2]<<endl;
+    cout<<Registers[4]<<": "<<RegisterValues[4]<<endl;
+    cout<<Registers[16]<<": "<<RegisterValues[16]<<endl;
+    cout<<Registers[29]<<": "<<RegisterValues[29]<<endl;
+    cout<<Registers[31]<<": "<<RegisterValues[31]<<endl;
 
-        ReadInstruction(ProgramCounter); //set current_instruction
-        int32_t instruction_op = ParseInstruction(); // return op , populate values in r[]
-        ExecuteInstruction(instruction_op); //call appropriate operation function based on the op and increment program counter
+    for (int i = 4070; i<=4096; i++){
+        cout<<"Memory"<<i<<"  :"<<Memory[i]<<endl;
     }
-    cout<<endl<<"Execution completed successfully"<<endl<<endl;
-    cout<<"r[1]:"<<RegisterValues[r[1]]<<endl;
 }
+
 
 
 /*set current_instruction*/
@@ -118,35 +133,53 @@ void MIPSSimulator::ReadInstruction(int32_t program_counter)
 }
 
 /*call appropriate operation function based on the op and increment program counter*/
-void MIPSSimulator::ExecuteInstruction(int32_t instruction_op)
+void MIPSSimulator::ExecuteInstruction()
 {
-    switch(instruction_op) 
+    cout<<Instruction_op<<endl;
+    switch(Instruction_op) 
     {
-        // case 0:
-        //     add();
-        //     break;
-        // case 1:
-        //     sub();
-        //     break;
-        // case 2:
-        //     mul();
-        //     break;
-        // case 3:
-        //     andf();
-        //     break;
-        // case 4:
-        //     orf();
-        //     break;
-        // case 5:
-        //     nor();
-        //     break;
-        // case 6:
-        //     slt();
-        //     break;
-        case 8:
+        //R-type
+        case 0: 
+            if(Instruction_funct == 32){
+                add();
+                ProgramCounter++;
+            }else if (Instruction_funct == 8){
+                jr();
+            }else if (Instruction_funct == 42){
+                slt();
+                ProgramCounter++;
+            }else{
+                ProgramCounter++;
+            }
+            break;
+
+        // I-type
+        case 5: 
+            bne();
+            ProgramCounter++;
+            break;
+        case 35:
+            lw();
+            ProgramCounter++;
+            break;
+        case 43:
+            sw();
+            ProgramCounter++;
+            break;
+        case 8: 
             addi();
             ProgramCounter++;
             break;
+        //J type
+        case 2: 
+            j();
+            // ProgramCounter++;
+            break;
+        case 3: 
+            jal();
+            // ProgramCounter++;
+            break;
+
         // case 8:
         //     andi();
         //     break;
@@ -156,18 +189,12 @@ void MIPSSimulator::ExecuteInstruction(int32_t instruction_op)
         // case 10:
         //     slti();
         //     break;
-        // case 11:
-        //     lw();
-        //     break;
-        // case 12:
-        //     sw();
-        //     break;
+
         // case 13:
         //     beq();
         //     break;
-        // case 14:
-        //     bne();
-        //     break;
+
+
         // case 15:
         //     j();
         //     break;
@@ -182,14 +209,69 @@ void MIPSSimulator::ExecuteInstruction(int32_t instruction_op)
     }
 }
 
-// opcode   rs  rt  IMM
+// R-type : opcode   rs  rt  rd
+void MIPSSimulator::add()
+{
+    RegisterValues[r[2]]=RegisterValues[r[0]]+RegisterValues[r[1]];
+}
+void MIPSSimulator::jr()
+{
+    ProgramCounter = RegisterValues[r[0]];
+}
+
+void MIPSSimulator::slt()
+{
+    if(RegisterValues[r[0]]<RegisterValues[r[1]]){
+        RegisterValues[r[2]] =1;
+    }else{
+        RegisterValues[r[2]] = 0;
+    }
+}
+// I-type : opcode   rs  rt  IMM
 void MIPSSimulator::addi()
 {
     RegisterValues[r[1]]=RegisterValues[r[0]]+r[2];
 }
 
+void MIPSSimulator::bne()
+{
+    if(RegisterValues[r[0]] != RegisterValues[r[1]]){
+        ProgramCounter += r[2]<<2 ;
+        int temp = r[2] << 2;
+        cout<<"bne jump: "<< temp <<endl;
+    }
+}
+
+void MIPSSimulator::lw()
+{
+    RegisterValues[r[1]]= Memory[r[2]+RegisterValues[r[0]]];
+}
+
+void MIPSSimulator::sw()
+{
+    cout<<"****sw :  "<<(RegisterValues[r[1]]+r[2])<<"   "<< RegisterValues[r[0]]<<endl;
+    Memory[RegisterValues[r[0]]+r[2]] =  RegisterValues[r[1]];
+}
+
+// J type
+void MIPSSimulator::j()
+{
+    ProgramCounter = r[0]<<2;
+}
+
+void MIPSSimulator::jal()
+{
+    RegisterValues[31] = ProgramCounter + 1;
+    // cout << "r0: " << r[0] <<endl;
+    ProgramCounter = r[0] <<2;
+    // cout << "PC aftre Jal: " << ProgramCounter <<endl;
+    // ProgramCounter = ProgramCounter << 2;
+    // cout << "PC aftre leftshift: " << ProgramCounter <<endl;
+}
+
+
 // return op , populate values in r[]
-int32_t MIPSSimulator::ParseInstruction()
+void MIPSSimulator::ParseInstruction()
 {
     if(current_instruction.size()!=32) 
     {
@@ -197,19 +279,72 @@ int32_t MIPSSimulator::ParseInstruction()
         // ReportError();
     }
 
-    int32_t OperationID = stoi(current_instruction.substr(0,6).c_str(), nullptr, 2);  
+    Instruction_op = stoi(current_instruction.substr(0,6).c_str(), nullptr, 2);  
+    Instruction_funct = stoi(current_instruction.substr(26,6).c_str(), nullptr, 2);  
 
-    if(OperationID == 8){
-    r[0] = stoi(current_instruction.substr(6,5).c_str(), nullptr, 2);
-    r[1] = stoi(current_instruction.substr(11,5).c_str(), nullptr, 2);
-    r[2] = stoi(current_instruction.substr(16,16).c_str(), nullptr, 2);
+    // I-type
+    if(Instruction_op == 8 || Instruction_op == 5 || Instruction_op == 35 || Instruction_op == 43){
+        r[0] = stoi(current_instruction.substr(6,5).c_str(), nullptr, 2);
+        r[1] = stoi(current_instruction.substr(11,5).c_str(), nullptr, 2);
+        // r[2] = stoi(current_instruction.substr(16,16).c_str(), nullptr, 2);
+
+
+        if(current_instruction[16] == '1'){
+            char temp_imm[17];
+            strcpy(temp_imm,current_instruction.substr(16,16).c_str());
+            for (int i = 0; i<16; i++){
+                if(temp_imm[i]=='1'){temp_imm[i]='0';}
+                else{temp_imm[i]='1';}
+            }
+            r[2] = stoi(temp_imm, nullptr, 2);
+            r[2] ++;
+            r[2] = 0-r[2];
+            cout<<"r[2]:"<<r[2]<<endl; 
+        }else{
+            r[2] = stoi(current_instruction.substr(16,16).c_str(), nullptr, 2);
+        }
     }
-    else{
-        cout<<"Error: Invalid OperationID"<<endl;
+    // R-type
+    else if(Instruction_op == 0){
+        r[0] = stoi(current_instruction.substr(6,5).c_str(), nullptr, 2);
+        r[1] = stoi(current_instruction.substr(11,5).c_str(), nullptr, 2);
+        r[2] = stoi(current_instruction.substr(16,5).c_str(), nullptr, 2);
+    // J-type
+    }else if (Instruction_op == 2 ||Instruction_op == 3){
+        r[0] = stoi(current_instruction.substr(6,26).c_str(), nullptr, 2);
+        // cout<<"r[0]: "<<r[0]<<endl;
+    }else{
+        cout<<"Error: Invalid Instruction_op"<<endl;
         // ReportError();   
     }
-    return OperationID;
 }
+
+//function to run the simulator
+void MIPSSimulator::Execute()
+{
+    char test = getchar(); //to remove effect of pressing enter key while starting
+    cout<<"getchar() in Execute: "<<test <<endl;
+    
+    int restr = 0;
+    int print_flag = 0;
+    while(ProgramCounter<NumberOfInstructions) 
+    // while(ProgramCounter<NumberOfInstructions) 
+    {
+        cout<<"PC: "<<ProgramCounter<<endl;
+        if (ProgramCounter%4 == 0){print_flag = 1;restr ++;}
+        ReadInstruction(ProgramCounter); //set current_instruction
+        ParseInstruction(); // return op , populate values in r[]
+        ExecuteInstruction(); //call appropriate operation function based on the op and increment program counter
+        if(print_flag == 1){PrintRegister(); }
+        
+        print_flag = 0;
+        cout<<"************ "<<endl;
+
+    }
+    cout<<endl<<"Execution completed successfully"<<endl<<endl;
+    cout<<"$v0:"<<RegisterValues[2]<<endl;
+}
+
 
 int main()
 {
